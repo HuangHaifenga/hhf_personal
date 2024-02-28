767c213df5f332ad2caa05eb8443dffc464ed2b2
黄海峰 <811373173@qq.com>
2024-02-28 10:05:02 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // 导入router所需的方法
-import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 // 导入路由页面的配置
 import routes from './routes'
@@ -11,7 +11,7 @@ const router = createRouter({
     routes: routes,
 })
 
-// 全局前置守卫，这里可以加入用户登录判断
+//  全局前置守卫，这里可以加入用户登录判断
 router.beforeEach((to, from, next) => {
     // 继续前进 next()
     // 返回 false 以取消导航
