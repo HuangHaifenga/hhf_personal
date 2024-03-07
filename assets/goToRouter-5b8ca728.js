df94eaae1ac1512c3d6140acc4448ae6ff87a009
HuangHaifenga <105699250+HuangHaifenga@users.noreply.github.com>
2024-03-07 08:15:51 +0000

    Deploying to gh-pages from  @ aef902eb9c9f5b511fd8bc76bb584d14ff643594 🚀

---
+++ assets/goToRouter-5b8ca728.js
@@ -0,0 +1,1 @@
+import{e}from"./index-0c95e28c.js";function u(){const o=e();return function(){o.push("/")}}export{u};
--- assets/goToRouter-ba6e9672.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./index-770dd754.js";function u(){const o=e();return function(){o.push("/")}}export{u};
